75a901793af7d6f2b8e73126e7ad8dc7de04c6a5
Rajesh chaubey <rajesh-kumar.chaubey@capgemini.com>
2024-10-24 19:33:37 +0530

    Update aem.js

--- scripts/aem.js
+++ scripts/aem.js
@@ -576,6 +576,7 @@ function buildBlock(blockName, content) {
  * @param {Element} block The block element
  */
 async function loadBlock(block) {
+  console.log('loadBlock')
   const status = block.dataset.blockStatus;
   if (status !== 'loading' && status !== 'loaded') {
     block.dataset.blockStatus = 'loading';
