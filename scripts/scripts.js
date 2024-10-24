053abd5186dedf35cab1d3812e000e381c800965
Rajesh chaubey <rajesh-kumar.chaubey@capgemini.com>
2024-10-24 17:00:01 +0530

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,34 +1,74 @@
 import {
-  sampleRUM,
-  buildBlock,
-  loadHeader,
-  loadFooter,
-  // decorateButtons,
-  addLoadingToHeader,
+  decorateBlocks,
   decorateIcons,
   decorateSections,
-  decorateBlocks,
   decorateTemplateAndTheme,
-  waitForLCP,
+  getMetadata,
   loadBlocks,
   loadCSS,
-} from './aem.js';
+  loadFooter,
+  loadHeader,
+  loadScript,
+  sampleRUM,
+  waitForLCP,
+} from './lib-franklin.js';
 
-const LCP_BLOCKS = []; // add your LCP blocks to the list
+const LCP_BLOCKS = ['section-hero', 'article-slides']; // add your LCP blocks to the list
 
-/**
- * Builds hero block and prepends to main in a new section.
- * @param {Element} main The container element
- */
-function buildHeroBlock(main) {
-  const h1 = main.querySelector('h1');
-  const picture = main.querySelector('picture');
-  // eslint-disable-next-line no-bitwise
-  if (h1 && picture && (h1.compareDocumentPosition(picture) & Node.DOCUMENT_POSITION_PRECEDING)) {
-    const section = document.createElement('div');
-    section.append(buildBlock('hero', { elems: [picture, h1] }));
-    main.prepend(section);
+export function linkSmallImagesToFullImages(container) {
+  for (const picture of container.querySelectorAll('.section.small-images picture')) {
+    if (picture.closest('div.block') || picture.closest('a')) {
+      // don't link if already in a block or a link.
+      // eslint-disable-next-line no-continue
+      continue;
+    }
+
+    const link = document.createElement('a');
+    link.href = picture.querySelector('img').src;
+    link.innerHTML = picture.outerHTML;
+    picture.replaceWith(link);
   }
+}
+
+export function decorateLinkedPictures(container, processInBlocks = true) {
+  /* MS Word online does not support linked images. As a workaround use any links
+  that are directly after the image. */
+
+  // picture + br + a in the same paragraph
+  [...container.querySelectorAll('picture + br + a, picture + a')]
+  // link text is an unformatted URL paste
+    .filter((a) => a.textContent.trim().startsWith('http'))
+    .forEach((a) => {
+      const br = a.previousElementSibling;
+      let picture = br.previousElementSibling;
+      if (br.tagName === 'PICTURE') picture = br;
+      picture.remove();
+      br.remove();
+      a.innerHTML = picture.outerHTML;
+      // make sure the link is not decorated as a button
+      a.parentNode.classList.remove('button-container');
+      a.className = '';
+    });
+
+  // with link and image in separate paragraphs
+  [...container.querySelectorAll('p > a[href]')]
+  // don't decorate if already in a block. Instead, the block should call this function.
+    .filter((a) => !a.closest('div.block') || processInBlocks)
+  // link (in a <p>) has no siblings
+    .filter((a) => a.parentNode.childElementCount === 1)
+  // is preceded by an image (in a <p>) and image has no other siblings
+    .filter((a) => a.parentNode.previousElementSibling?.firstElementChild?.tagName === 'PICTURE')
+    .filter((a) => a.parentNode.previousElementSibling?.childElementCount === 1)
+  // link text is an unformatted URL paste
+    .filter((a) => a.textContent.trim().startsWith('http'))
+    .forEach((a) => {
+      const picture = a.parentNode.previousElementSibling.firstElementChild;
+      picture.parentNode.remove();
+      a.innerHTML = picture.outerHTML;
+      // make sure the link is not decorated as a button
+      a.parentNode.classList.remove('button-container');
+      a.className = '';
+    });
 }
 
 /**
@@ -43,13 +83,68 @@ async function loadFonts() {
   }
 }
 
+export function getYoutubeVideoId(url) {
+  if (url.includes('youtube.com/watch?v=')) {
+    return new URL(url).searchParams.get('v');
+  }
+  if (url.includes('youtube.com/embed/') || url.includes('youtu.be/')) {
+    return new URL(url).pathname.split('/').pop();
+  }
+  return null;
+}
+
+function decorateVideoLinks(main) {
+  [...main.querySelectorAll('a')]
+    .filter(({ href }) => !!href)
+  // only convert plain links
+    .filter((a) => a.textContent?.trim()?.toLowerCase().startsWith('http'))
+  // don't decorate if already in a block. unless it's `columns`.
+    .filter((a) => {
+      const block = a.closest('div.block');
+      if (!block) return true;
+      return block.classList.contains('columns');
+    })
+    .forEach((link) => {
+      const youtubeVideoId = getYoutubeVideoId(link.href);
+
+      if (youtubeVideoId) {
+        loadCSS(`${window.hlx.codeBasePath}/blocks/embed/lite-yt-embed.css`);
+        loadScript(`${window.hlx.codeBasePath}/blocks/embed/lite-yt-embed.js`);
+        const video = document.createElement('lite-youtube');
+        video.setAttribute('videoid', youtubeVideoId);
+        video.setAttribute('params', 'rel=0');
+        video.classList.add('youtube-video');
+        link.replaceWith(video);
+      }
+    });
+}
+
+function decorateSpotifyLinks(main) {
+  [...main.querySelectorAll('a')]
+    .filter(({ href }) => href.includes('open.spotify.com/embed'))
+  // don't decorate if already in a block.
+    .filter((a) => !a.closest('div.block'))
+    .forEach((link) => {
+      const iframe = document.createElement('iframe');
+      iframe.classList.add('spotify-embed');
+      iframe.width = 800;
+      iframe.height = 300;
+      iframe.src = link.href;
+      iframe.title = link.textContent;
+      iframe.frameborder = 0;
+      iframe.allow = 'encrypted-media';
+      iframe.allowfullscreen = true;
+      link.replaceWith(iframe);
+    });
+}
+
 /**
  * Builds all synthetic blocks in a container element.
  * @param {Element} main The container element
  */
 function buildAutoBlocks(main) {
   try {
-    buildHeroBlock(main);
+    decorateLinkedPictures(main, false);
   } catch (error) {
     // eslint-disable-next-line no-console
     console.error('Auto Blocking failed', error);
@@ -62,12 +157,16 @@ function buildAutoBlocks(main) {
  */
 // eslint-disable-next-line import/prefer-default-export
 export function decorateMain(main) {
-  // hopefully forward compatible button decoration
+  // 24life only uses buttons in article template, so we call this from there instead
   // decorateButtons(main);
+
   decorateIcons(main);
-  // buildAutoBlocks(main);
+  buildAutoBlocks(main);
   decorateSections(main);
   decorateBlocks(main);
+  decorateVideoLinks(main);
+  decorateSpotifyLinks(main);
+  linkSmallImagesToFullImages(main);
 }
 
 /**
@@ -77,9 +176,23 @@ export function decorateMain(main) {
 async function loadEager(doc) {
   document.documentElement.lang = 'en';
   decorateTemplateAndTheme();
+  if (!document.title.match(/- 24Life(\.com)?$/i)) {
+    document.title += ' - 24Life';
+  }
+  // set global accent color
+  const section = getMetadata('section')?.toLowerCase();
+  if (section) {
+    document.body.style.setProperty('--accent-color', `var(--color-${section})`);
+  } else {
+    document.body.style.setProperty('--accent-color', 'var(--color-default-card)');
+  }
   const main = doc.querySelector('main');
   if (main) {
     decorateMain(main);
+    const templateName = getMetadata('template');
+    if (templateName) {
+      await loadTemplate(doc, templateName.toLowerCase());
+    }
     document.body.classList.add('appear');
     await waitForLCP(LCP_BLOCKS);
   }
@@ -99,7 +212,6 @@ async function loadEager(doc) {
  * @param {Element} doc The container element
  */
 async function loadLazy(doc) {
-  const spinner = addLoadingToHeader(doc.querySelector('header'))
   const main = doc.querySelector('main');
   await loadBlocks(main);
 
@@ -107,9 +219,7 @@ async function loadLazy(doc) {
   const element = hash ? doc.getElementById(hash.substring(1)) : false;
   if (hash && element) element.scrollIntoView();
 
-  loadHeader(doc.querySelector('header')).then(() => {
-    spinner.remove()
-  });;
+  loadHeader(doc.querySelector('header'));
   loadFooter(doc.querySelector('footer'));
 
   loadCSS(`${window.hlx.codeBasePath}/styles/lazy-styles.css`);
@@ -137,3 +247,26 @@ async function loadPage() {
 }
 
 loadPage();
+
+// from https://www.hlx.live/developer/block-collection
+async function loadTemplate(doc, templateName) {
+  try {
+    const cssLoaded = loadCSS(`${window.hlx.codeBasePath}/templates/${templateName}/${templateName}.css`);
+    const decorationComplete = (async () => {
+      try {
+        const mod = await import(`../templates/${templateName}/${templateName}.js`);
+        if (mod.default) {
+          await mod.default(doc);
+        }
+      } catch (error) {
+        // eslint-disable-next-line no-console
+        console.log(`failed to load module for ${templateName}`, error);
+      }
+    })();
+
+    await Promise.all([cssLoaded, decorationComplete]);
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.log(`failed to load block ${templateName}`, error);
+  }
+}
