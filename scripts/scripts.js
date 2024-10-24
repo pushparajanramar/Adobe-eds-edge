d9c01a0f076f102234c7e92a17e93db6314c9620
Rajesh chaubey <rajesh-kumar.chaubey@capgemini.com>
2024-10-24 19:06:28 +0530

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -137,3 +137,21 @@ async function loadPage() {
 }
 
 loadPage();
+export function decorateMain(main) {
+    // hopefully forward compatible button decoration
+    decorateButtons(main);
+    decorateIcons(main);
+    buildAutoBlocks(main);
+    decorateSections(main);
+    decorateBlocks(main);
+    decorateBookmarkLinks(main)
+}
+ 
+/**
+* Decorates decorate BookmarkLinks links to absolute.
+* @param {Element} main The container element
+*/
+export function decorateBookmarkLinks(main) {
+    main.querySelectorAll('a').forEach((a) => {
+        const href = a.getAttribute('href'); if (href) { // Check if the href contains 'bookmark' if (href.includes('bookmark')) { // Convert to a relative URL by removing the domain const relativeUrl = href.replace(/^.*\/\/[^\/]+/, ''); a.setAttribute('href', relativeUrl); console.log(`Converted to relative URL: ${relativeUrl}`); } } });
+        }
