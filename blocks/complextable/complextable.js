d7317671067183c47393f372a0b898e6d4aef1bf
Rajesh chaubey <rajesh-kumar.chaubey@capgemini.com>
2024-10-29 21:50:47 +0530

    Update complextable.js

--- blocks/complextable/complextable.js
+++ blocks/complextable/complextable.js
@@ -70,25 +70,7 @@ function buildCell(rowIndex) {
 }
  
 export default async function decorate(block) {
-  const table = document.createElement("table");
-  const thead = document.createElement("thead");
-  const tbody = document.createElement("tbody");
-  const header = !block.classList.contains("no-header");
-  if (header) table.append(thead);
-  table.append(tbody);
-  
-  [...block.children].forEach((child, i) => {
-    const row = document.createElement("tr");
-    if (header && i === 0) thead.append(row);
-    else tbody.append(row);
-    
-    [...child.children].forEach((col) => {
-      const cell = buildCell(header ? i : i + 1);
-      cell.innerHTML = col.innerHTML;
-      row.append(cell);
-    });
-  });
-   const tableDivs = document.querySelectorAll(".complextable div");
+  const tableDivs = document.querySelectorAll(".complextable div");
   const newDiv = document.createElement('div');
 
   // Move all divs to newDiv
