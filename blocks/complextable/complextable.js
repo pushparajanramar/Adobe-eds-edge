a8645c60f2f4e53219b2651935bd8ead9422eed2
Rajesh chaubey <rajesh-kumar.chaubey@capgemini.com>
2024-10-29 11:21:46 +0530

    Update complextable.js
    block convert to code

--- blocks/complextable/complextable.js
+++ blocks/complextable/complextable.js
@@ -1,1 +1,9 @@
-
+export default async function decorate(block) {
+  console.log(block,"block")
+  const table = document.createElement("table");
+  const thead = document.createElement("thead");
+  const tbody = document.createElement("tbody");
+  const header = !block.classList.contains("no-header");
+  if (header) table.append(thead);
+  table.append(tbody);
+}
