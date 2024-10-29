6829b2bb12e96fe1ab2e2b1b057324e304422ab6
Rajesh chaubey <rajesh-kumar.chaubey@capgemini.com>
2024-10-29 13:07:59 +0530

    Update complextable.js

--- blocks/complextable/complextable.js
+++ blocks/complextable/complextable.js
@@ -1,4 +1,25 @@
-export default async function decorate(block) {
-  console.log("complextable is working here ")
+
+
+  export default async function decorate(block) {
+      console.log("complextable is working here ")
+  const table = document.createElement("table");
+  const thead = document.createElement("thead");
+  const tbody = document.createElement("tbody");
+  const header = !block.classList.contains("no-header");
+  if (header) table.append(thead);
+  table.append(tbody);
+
+  [...block.children].forEach((child, i) => {
+    const row = document.createElement("tr");
+    if (header && i === 0) thead.append(row);
+    else tbody.append(row);
+    [...child.children].forEach((col) => {
+      const cell = "rajesh";
+      cell.innerHTML = col.innerHTML;
+      row.append(cell);
+    });
+  });
+  block.innerHTML = "";
+  block.append(table);
 }
 
