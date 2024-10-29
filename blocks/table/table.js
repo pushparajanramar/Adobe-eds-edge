55ce83a370d9fd6cd0f44fdb7b773981d1067fe8
Rajesh chaubey <rajesh-kumar.chaubey@capgemini.com>
2024-10-29 12:40:43 +0530

    Update table.js

--- blocks/table/table.js
+++ blocks/table/table.js
@@ -6,9 +6,12 @@ function buildCell(rowIndex, isHeader, colspan) {
 }
 
 export default async function decorate(block) {
-   const table = document.createElement("table");
-   const thead = document.createElement("thead");
+    const table = document.createElement("table");
+  const thead = document.createElement("thead");
   const tbody = document.createElement("tbody");
+  const header = !block.classList.contains("no-header");
+  if (header) table.append(thead);
+  table.append(tbody);
   
    const tables = [...block.querySelectorAll(".table-wrapper .table")];
   console.log('tables',tables)
